0376af509305b07edda8727889d63a195dee1c80
Robert Schwandner <robert@kohlenberg.info>
2017-04-15 07:47:36 +0200

    handle local files and remote urls

--- code/KrakenService.php
+++ code/KrakenService.php
@@ -1,172 +1,189 @@
 <?php
-require_once (BASE_PATH . '/vendor/kraken-io/kraken-php/lib/Kraken.php');
 
 /**
  * the kraken service handles authenication to the kraken
  * api and provides functions for calling the api
  * @author Ryan
  */
-class KrakenService {
-    
+class KrakenService
+{
+
     /**
      * api key used for future requests
-     * @var type 
+     * @var type
      */
     protected $apiKey = null;
-    
+
     /**
      * api secret used for future requests
-     * @var type 
+     * @var type
      */
     protected $apiSecret = null;
-    
+
     /**
-     * 
-     * @var type 
+     *
+     * @var type
      */
     protected $krakenConnection = null;
-    
-    function __construct(){
+
+    function __construct()
+    {
         $siteConfig = SiteConfig::current_site_config();
-                
+
         $this->apiKey = $siteConfig->KrakenAPIKey;
         $this->apiSecret = $siteConfig->KrakenAPISecret;
     }
-    
+
     /**
      * establish connection to the Kraken API
      * @return Kraken
      */
-    public function getConnection(){
-        if($this->krakenConnection){
+    public function getConnection()
+    {
+        if ($this->krakenConnection) {
             return $this->krakenConnection;
         }
-        
+
         //establish connection if api keys are set
-        if($this->checkApiKeys()){            
+        if ($this->checkApiKeys()) {
             $kraken = new Kraken($this->apiKey, $this->apiSecret);
-            
+
             $this->krakenConnection = $kraken;
-            
+
             return $this->krakenConnection;
         }
     }
-    
+
     /**
      * optimize an image
      * @param String $file
      * @return Array
      */
-    public function optimizeImage($file){
+    public function optimizeImage($file)
+    {
         $kraken = $this->getConnection();
-        
-        if($kraken){
-        
+
+        if ($kraken) {
+
             $params = array(
-                "file" => $file,
                 "lossy" => $this->getLossy(),
-				"dev" => $this->getDev()
-            );						
+                "dev"   => $this->getDev()
+            );
 
-            if($this->getWait()){
+            if ($this->getWait()) {
                 $params['wait'] = true;
-            }     
-            
-            set_time_limit(400);            
-            $data = $kraken->upload($params);
+            }
 
+            set_time_limit(400);
+
+            // local file or remote url
+            if (strpos($file, 'http') === 0) {
+                $params['url'] = $file;
+                $data = $kraken->url($params);
+            } else {
+                $params['file'] = $file;
+                $data = $kraken->upload($params);
+            }
             return $data;
         }
     }
-    
+
     /**
      * Uses cURL to fetch an optimized image from Kraken
      * @param String $krakedUrl
      * @return JSON
      */
-    public function getOptimizedImage($krakedUrl){
-        $ch = curl_init ($krakedUrl);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
-        curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
-        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 0 ); 
-        curl_setopt( $ch, CURLOPT_TIMEOUT, 60 );           	      
-        curl_setopt( $ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36' );
-       
-        $result = curl_exec( $ch );
-        
+    public function getOptimizedImage($krakedUrl)
+    {
+        $ch = curl_init($krakedUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36');
+
+        $result = curl_exec($ch);
+
         if ($result) {
             return $result;
-        }       
+        }
     }
-    
+
     /**
      * return wait option
      * @return Boolean
      */
-    public function getWait(){
+    public function getWait()
+    {
         return Config::inst()->get('Kraken', 'wait');
     }
-    
+
     /**
      * set the lossy option
      * @param Boolean $lossy
      */
-    public function setLossy($lossy){
+    public function setLossy($lossy)
+    {
         Config::inst()->update('Kraken', 'lossy', $lossy);
     }
-    
+
     /**
      * return the lossy option
      * @return type
      */
-    public function getLossy(){
+    public function getLossy()
+    {
         return Config::inst()->get('Kraken', 'lossy');
     }
-	
-	/**
-	 * return the Kraken dev config option
-	 * @return bool
-	 */
-	public function getDev(){
-		return Config::inst()->get('Kraken', 'dev');
-	}
-	
-	/**
-	 * if this is set to true then reoptimize all images, regardless
-	 * of their Kraked status. Useful if you need to make sure
+
+    /**
+     * return the Kraken dev config option
+     * @return bool
+     */
+    public function getDev()
+    {
+        return Config::inst()->get('Kraken', 'dev');
+    }
+
+    /**
+     * if this is set to true then reoptimize all images, regardless
+     * of their Kraked status. Useful if you need to make sure
      * all images are optimizes
-	 * @return bool
-	 */
-	public function getIgnoreKrakedStatus(){
-		return Config::inst()->get('Kraken', 'ignore_kraked_status');
-	}
-    
+     * @return bool
+     */
+    public  function getIgnoreKrakedStatus()
+    {
+        return Config::inst()->get('Kraken', 'ignore_kraked_status');
+    }
+
     /**
      * check if the API key and API secret have been
      * configured
      * @return boolean
      */
-    private function checkApiKeys(){
-        if($this->apiKey === null || $this->apiSecret === null){
+    private function checkApiKeys()
+    {
+        if ($this->apiKey === null || $this->apiSecret === null) {
             return user_error('The Kraken API Key and API Secret have not been configured in SiteConfig.', E_USER_ERROR);
-        }else{
+        } else {
             return true;
         }
     }
-    
+
     /**
      * check api status
      * @return boolean
      */
-    public function getAPIStatus(){        
-        if($this->apiKey && $this->apiSecret){            
-            $kraken = new Kraken( $this->apiKey, $this->apiSecret );
+    public function getAPIStatus()
+    {
+        if ($this->apiKey && $this->apiSecret) {
+            $kraken = new Kraken($this->apiKey, $this->apiSecret);
             $status = $kraken->status();
             return $status;
         }
-        
+
         return false;
     }
-    
+
 }
